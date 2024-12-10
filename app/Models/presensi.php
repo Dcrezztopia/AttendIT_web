24f391f54edb4d8aca833e95860780f83142e383
mulkihakim <mulkihkm@gmail.com>
2024-12-10 10:58:35 +0700

    kapitalmodel

--- app/Models/presensi.php
+++ app/Models/presensi.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class presensi extends Model
+class Presensi extends Model
 {
     use HasFactory;
 
